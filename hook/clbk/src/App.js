7ae6eb87ab84962b4918797087a7e286acc9295c
karthi <karthickparamasivam03@gmail.com>
2024-04-12 14:06:42 +0530

    tasks

---
+++ hook/clbk/src/App.js
@@ -0,0 +1,12 @@
+import Counter from './Clac.js';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Counter />
+      </header>
+    </div>
+  );
+}
+
+export default App;
